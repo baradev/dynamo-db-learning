7ad4e8cb3622a6c731ad980a3722938d50e40f05
Barbora Klusackova <barboraklusackova@gmail.com>
2024-07-18 09:49:35 +1200

    update global css

--- my-nextjs-dynamo-app/src/app/(root)/page.tsx
+++ my-nextjs-dynamo-app/src/app/(root)/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Human Skills Learninf Platform </div>
+  return (
+    <section className="home">
+      <div>Human Skills Learninf Platform </div>
+    </section>
+  )
 }
 
 export default Home
